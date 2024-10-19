bb84652b0c8484f4f8bf7eef26ee24c9a2e9ffac
Justin Worrall <justin.worrall@gmail.com>
2024-10-19 19:46:27 +0100

    bumped to 0.1.2

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def filter_packages(root):
     
 setuptools.setup(
     name="davai",
-    version="0.1.1",
+    version="0.1.2",
     author="jhw",
     author_email="justin.worrall@gmail.com",
     description="A cli- based AI agent for developing front end apps",
